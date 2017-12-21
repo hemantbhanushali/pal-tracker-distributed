d1a725a295006e58ecee85dbceeb28e8d20b1b62
Hemant Bhanushali <hemant.bhanushali@mastercard.com>
2017-12-21 17:08:02 -0500

    Exclude Rabbit AutoDiscovery

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -2,13 +2,14 @@ package io.pivotal.pal.tracker.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 import java.util.TimeZone;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RabbitAutoConfiguration.class)
 @EnableEurekaClient
 @ComponentScan({
     "io.pivotal.pal.tracker.accounts",
